cbc752f36c1795080a5bca0167086fe59945a294
Dev-Souza <kauan.souza.dev@gmail.com>
2025-06-06 19:11:44 -0300

    feat: add route the drawer in login

--- src/routes/LoginRegisterStackRoutes.jsx
+++ src/routes/LoginRegisterStackRoutes.jsx
@@ -3,24 +3,29 @@ import { createStackNavigator } from '@react-navigation/stack'
 // Importações
 import RegisterUserScreen from '../screens/users/register/RegisterUserScreen'
 import LoginScreen from '../screens/users/login/LoginScreen'
+import DrawerMainScreenRoutes from './DrawerMainScreenRoutes'
 
 const Stack = createStackNavigator()
 
 export default function LoginRegisterStackRoutes() {
   return (
     <Stack.Navigator>
-      <Stack.Screen 
-        name='ListaScreen' 
-        component={LoginScreen} 
+      <Stack.Screen
+        name='ListaScreen'
+        component={LoginScreen}
         options={{
           headerShown: false
-        }}/>
-      <Stack.Screen 
-        name='RegisterUserScreen' 
-        component={RegisterUserScreen} 
+        }} />
+      <Stack.Screen
+        name='RegisterUserScreen'
+        component={RegisterUserScreen}
         options={{
           headerTitle: "Criar Usuário"
-        }}/>
+        }} />
+      <Stack.Screen
+        name="Drawer"
+        component={DrawerMainScreenRoutes}
+        options={{ headerShown: false }} />
     </Stack.Navigator>
   )
 }
