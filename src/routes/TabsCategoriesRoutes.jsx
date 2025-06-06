ef815c7fba05221421ae2d1c6b9e4dd3f01f1fed
Dev-Souza <kauan.souza.dev@gmail.com>
2025-06-06 20:12:55 -0300

    feat: create tabs the categories

---
+++ src/routes/TabsCategoriesRoutes.jsx
@@ -0,0 +1,34 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { Ionicons } from '@expo/vector-icons';
+
+import ListCategoriesScreen from '../screens/categories/ListCategoriesScreen';
+import NewCategoryScreen from '../screens/categories/NewCategoryScreen';
+
+const Tab = createBottomTabNavigator();
+
+export default function TabsCategoriesRoutes() {
+    return (
+        <Tab.Navigator screenOptions={{ headerShown: false }}>
+            <Tab.Screen
+                name="ListCategorias"
+                component={ListCategoriesScreen}
+                options={{
+                    title: "Lista Categorias",
+                    tabBarIcon: ({ color, size }) => (
+                        <Ionicons name="list-outline" color={color} size={size} />
+                    ),
+                }}
+            />
+            <Tab.Screen
+                name="NewCategoryScreen"
+                component={NewCategoryScreen}
+                options={{
+                    title: "Nova Categoria",
+                    tabBarIcon: ({ color, size }) => (
+                        <Ionicons name="add-circle-outline" color={color} size={size} />
+                    ),
+                }}
+            />
+        </Tab.Navigator>
+    )
+}
