9dd99e33bce0de1f675cbc042a3964dc3858dd85
Dev-Souza <kauan.souza.dev@gmail.com>
2025-06-09 10:27:26 -0300

    fix: baseURL to work

--- src/services/toDoListService.js
+++ src/services/toDoListService.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 const toDoListService = axios.create({
-  // baseURL: 'http://10.30.32.84:8080/'
-  baseURL: "http://192.168.100.4:8080"
+  // baseURL: 'http://10.30.32.84:8080/' IESB
+  // baseURL: "http://192.168.100.4:8080" CASA
+  baseURL: "http://192.168.1.128:8080" // Trabalho
 });
 
 export default toDoListService;
